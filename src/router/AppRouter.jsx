4223e9c0386fdf21bb23a93e6a3acafb3682b098
Carolina <caroflorez000@gmail.com>
2023-01-02 16:01:02 -0500

    AppRoute Y CheckingAuth

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,29 @@
+import { async } from "@firebase/util";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
+import { FirebaseAuth } from "../firebase/config";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
+import { CheckingAuth } from "../ui/";
 
 export const AppRouter = () => {
+
+  const {status} = useSelector(state => state.auth);
+
+  useEffect(() => {
+    onAuthStateChanged(FirebaseAuth, async(user) => {
+
+    })
+
+  }, [])
+  
+
+  if(status === 'checking') {
+    return <CheckingAuth/>
+  }
+
   return (
     <Routes>
         <Route path="/auth/*" element={<AuthRoutes/>}/>
