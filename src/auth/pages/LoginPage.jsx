cfa32ed2066a2fa9634dadf53d70038a2fce00aa
Carolina <caroflorez000@gmail.com>
2023-01-10 16:19:40 -0500

    Valiaciones autenticación y inicio ForeBase

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -17,8 +17,8 @@ export const LoginPage = () => {
   const dispatch = useDispatch();
 
   const {email, password, onInputChange} = useForm({
-    email: 'carolina@google.com',
-    password: '123456'
+    email: '',
+    password: ''
   });
 
   const isAuthenticating = useMemo( () => status === 'checking', [status]);
